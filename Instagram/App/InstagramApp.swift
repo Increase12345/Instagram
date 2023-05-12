48745f09492e60625feee375c7f35be070788294
Nick <pretki123@ukr.net>
2023-05-12 13:19:01 -0600

    Getting started

---
+++ Instagram/App/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Nick Pavlov on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
