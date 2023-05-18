2c3bdc35845f4efe598195a75223fbc21c2f35a3
Nick <pretki123@ukr.net>
2023-05-18 15:07:14 -0600

    fixing bug

--- Instagram/App/InstagramApp.swift
+++ Instagram/App/InstagramApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import FirebaseCore
+import Firebase
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
@@ -21,6 +22,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 @main
 struct InstagramApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
